ed8a735737fda9602ea3c3dcf36ed1b47fd20e75
Andrey B <andreybars20@gmail.com>
2020-07-15 19:05:19 +0300

    check All worker!

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 import TodoList from "./components/List/todoList";
@@ -15,7 +15,7 @@ function App() {
     const [todos, setTodos] = React.useState([]);  //содержит в себе список todos первоначальный
     const [value, setValue] = React.useState('');  // хранит текущее значение input
     const [stateButton, setStateButton] = React.useState('All');
-    const [checkAll, setCheckAll] = React.useState('false');
+    const [checkAll, setCheckAll] = React.useState(false);
 
 
     const setTab = (e) => {   //установка текущего таба     //работает
@@ -26,27 +26,71 @@ function App() {
 
     // изменение статуса выбранных элементов
     const changeItemStatus = (item, check) => {
-
-
         console.log(item.id + " " + item.name);
         const newStatus = check.target.checked;
         const newTodoList = todos.map((el) => el.id === item.id ? {...el, status: newStatus} : el);
+        // let isAllChecked = false;
+        // newTodoList.forEach(el => {
+        //
+        // })
         setTodos(newTodoList);
         // здесь будет изменение общего количества элементов (counter)
     }
 
+    useEffect(() => {
+        console.log('sada');
+        const isCheckedAll = (el, index, array) => {
+            console.log(el.status, '!!');
+            return el.status
+        }
+        if(todos.length !== 0){
+            console.log('@@@',todos);
+            let every = todos.every(isCheckedAll)
+            console.log(every, '%%%');
+            if (every) {
+                setCheckAll(true)
+            } else {
+                setCheckAll(false)
+            }
+        } else setCheckAll(false);
+
+    },[todos, checkAll] );
+
+    useEffect(() => {
+        console.log('checkall', checkAll);
+    }, [checkAll])
+
+    /*useEffect(() => {
+        let validate = checkAll;
+        if (todo.length !== 0) todo.every((item) => (item.done ? (validate = true) : (validate = false)));
+        else validate = false;
+        setCheckAll(validate);
+    }, [todo, value, checkAll]);*/
+
+
     //изменение статусов всех элементов
     const changeAllStatus = (check) => {
         const newStatus = check.target.checked;
-
         if(newStatus){  //если true, то ищем элементы false и меняем на true
+            console.log(todos);
             const arr = todos.map(el => el.status !== true ? {...el, status: true} : el);
             setTodos(arr);
+            setCheckAll(true);
         }else {  //иначе, ищем элементы true и меняем на false
             const arr = todos.map(el => el.status !== false ? {...el, status: false} : el);
             setTodos(arr);
+            setCheckAll(false);
         }
+        // setCheckAll(newStatus);
     }
+
+    /*const changeAllStatus = (check) => {
+        const checked = check.target.checked;
+        const arr = todos.map((item) => ({ ...item, done: checked }));
+        setTodos(arr);
+        setCheckAll(checked);
+
+    };*/
 
 
     // метод возвращает лист todos в зависимости от текущего таба
@@ -100,17 +144,14 @@ function App() {
     const bClear = () => {
         const arr = todos.filter((el) => el.status !== true);
         setTodos(arr);
-
     }
-
-
-
 
 
   return (<div onKeyDown={handleKeyDown}>
           <header>
               <h1>Todos</h1>
               <Checkbox
+                  checked={checkAll}
                   onChange = {(check) => changeAllStatus(check)}
               />
               <Input
